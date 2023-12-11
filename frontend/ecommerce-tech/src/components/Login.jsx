8146b29542bfb9eca88bac95aa207a11e499e15e
Carlos Esposito <elcharlybeto@hotmail.com>
2023-12-11 19:33:57 -0300

    CSS for Active Links in Header

--- frontend/ecommerce-tech/src/components/Login.jsx
+++ frontend/ecommerce-tech/src/components/Login.jsx
@@ -1,10 +1,18 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+const getClassName = ({ isActive }) => {
+  if (isActive) {
+    return "text-slate-200 bg-purple-900 px-8 h-8 text-center rounded-2xl border border-slate-900";
+  }else{
+    return "text-purple-900 px-8 h-8 text-center rounded-2xl border border-purple-900 transition-all hover:text-slate-200 hover:bg-purple-900"
+  }
+}
 
 const Login = () => {
   return (
       <div className="flex w-[420px] place-content-around items-center gap-2">
-        <Link className="text-purple-900 px-8 h-8 text-center rounded-2xl border border-purple-900 transition-all hover:text-slate-200 hover:bg-purple-900" to={"/login"}>Ingresa con tu cuenta</Link>
-        <Link  className="text-purple-900 px-8 h-8 text-center rounded-2xl border border-purple-900 transition-all hover:text-slate-200 hover:bg-purple-900" to={"/register"}>¡Regístrate aquí!</Link>
+        <NavLink className={getClassName} to={"/login"}>Ingresa con tu cuenta</NavLink>
+        <NavLink  className={getClassName} to={"/register"}>¡Regístrate aquí!</NavLink>
       </div>
   );
 };
